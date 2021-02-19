83590999e91395f7a3c369d3f4b17f546e2fb9b9
Aman0802 <aman.khubani@gmail.com>
2021-02-19 22:18:09 +0530

    added the search functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -16,12 +17,16 @@ function App() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
 
-  // console.log("Term" + term);
-  // console.log(process.env.REACT_APP_PIXABAY_API_KEY);
   return (
     <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto mt-32">
+          No images found....
+        </h1>
+      )}
       {isLoading ? (
         <h1 className="text-6xl text-center mx-auto mt-32">Loading....</h1>
       ) : (
